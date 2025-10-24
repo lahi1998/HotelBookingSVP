03ca567c8a77838f48786bbd43bf77f4f2d029e5
lahi1998 <lars.sadolin.hinge@gmail.com>
2025-10-24 16:23:29 +0200

    login with observer, interface and service example.

--- Hotel-Angular/src/app/components/login/login.ts
+++ Hotel-Angular/src/app/components/login/login.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observer } from 'rxjs'
 import { LoginService } from '../../services/login-service';
+import { LoginInterface } from '../../interfaces/login-interface';
 
 
 @Component({
@@ -10,11 +11,44 @@ import { LoginService } from '../../services/login-service';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login implements OnInit{
-  constructor(private loginService: LoginService, private router: Router) {}
+export class Login {
+  username: string = '';
+  password: string = '';
+  hidePassword: boolean = true;
 
-  ngOnInit(): void {
+  usernameError: boolean = false;
+  passwordError: boolean = false;
 
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  togglePassword() {
+    this.hidePassword = !this.hidePassword;
   }
 
+  onSubmit() {
+    // Reset error flags
+    this.usernameError = !this.username.trim();
+    this.passwordError = !this.password.trim();
+
+    if (this.usernameError || this.passwordError) {
+      return; // Stop submission if any field is empty
+    }
+
+    const self = this;
+
+    const observer: Observer<LoginInterface> = {
+      next(response: LoginInterface) {
+        console.log('Login successful', response);
+        alert('Login successful!');
+        self.router.navigate(['/Components/mainContent']);
+      },
+      error(error) {
+        console.error('Login error', error);
+        alert('Invalid credentials.');
+      },
+      complete() { }
+    };
+
+    this.loginService.postLogin(this.username, this.password).subscribe(observer);
+  }
 }
