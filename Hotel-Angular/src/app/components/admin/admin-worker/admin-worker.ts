dccab44f5bb4513562fbd10f4c7dcd4103244c37
lahi1998 <lars.sadolin.hinge@gmail.com>
2025-10-27 15:28:29 +0100

    worker mangler logik og service, har laver ui delen og resposiv, også fixet responsiv for værelses typer.
    resposiv kunne nok godt bruge et touch op hvis der er tid.

--- Hotel-Angular/src/app/components/admin/admin-worker/admin-worker.ts
+++ Hotel-Angular/src/app/components/admin/admin-worker/admin-worker.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { worker } from '../../../interfaces/worker';
 
 @Component({
   selector: 'app-admin-worker',
@@ -7,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './admin-worker.css',
 })
 export class AdminWorker {
+  roles: string[] = ['Receptionist', 'Rengøring'];
+  displayedColumns: string[] = ['role', 'username', 'fullname', 'buttons'];
+  dataSource = new MatTableDataSource<worker>(DATA);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+// --- Mock data ---
+const DATA: worker[] = [
+  { role: 'Receptionist', username: 'Peje22', fullname: 'Peter hensen'},
+  { role: 'Receptionist', username: 'Lahi2', fullname: 'Lars hinge'},
+  { role: 'Rengøring', username: 'Pepe25', fullname: 'Peter pedersen'},
+  { role: 'Rengøring', username: 'Pila22', fullname: 'Pippi langstrømpe'},
+  { role: 'Receptionist', username: 'Mila1', fullname: 'Michael laudrup'},
+  { role: 'Receptionist', username: 'Pesc3', fullname: 'Peter Scmeichel'}
+];
+
--- Hotel-Angular/src/app/interfaces/worker.ts
+++ Hotel-Angular/src/app/interfaces/worker.ts
@@ -1,2 +1,5 @@
 export interface worker {
+    role: string;
+    username: string;
+    fullname: string;
 }
