8f81b4d51b288f9326cbc06be87381bfb540cc5a
lahi1998 <lars.sadolin.hinge@gmail.com>
2025-10-30 12:23:22 +0100

    started on cleaning page, the one where you see cleaning tasks and can mark them clean.

--- Hotel-Angular/src/app/components/staff/staffCleaning/staffCleaning.ts
+++ Hotel-Angular/src/app/components/staff/staffCleaning/staffCleaning.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { StaffService } from '../../../services/staffService';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { roomDto } from '../../../interfaces/roomDto';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-staffCleaning',
@@ -7,5 +12,44 @@ import { Component } from '@angular/core';
   styleUrl: './staffCleaning.css',
 })
 export class StaffCleaning {
+  displayedColumns: string[] = ['number', 'floor', 'type', 'bedCount', 'lastCleaned', 'roomStatus'];
+  filterValue: string = '';
+  DATA: roomDto[] = [];
+  dataSource = new MatTableDataSource<roomDto>(this.DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  constructor(
+    private staffService: StaffService,
+  ) { }
+
+  /* search filter */
+  searchFilter() {
+    this.dataSource.filter = this.filterValue.trim().toLowerCase();
+  }
+
+  getRooms() {
+    const observer: Observer<roomDto[]> = {
+      next: (rooms) => {
+        this.DATA = Array.isArray(rooms) ? rooms : [];
+        this.dataSource.data = this.DATA;
+        console.log('Rooms fetched successfully', rooms);
+        alert('Rooms fetched!');
+      },
+      error: (err) => {
+        console.error('Rooms fetch failed:', err);
+        alert('Rooms fetch failed!');
+      },
+      complete: () => {
+        // optional cleanup or navigation
+      },
+    };
+
+    this.staffService.getRooms().subscribe(observer);
+  }
 
 }
