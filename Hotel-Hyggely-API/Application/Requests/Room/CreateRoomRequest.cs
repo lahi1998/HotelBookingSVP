e3e2f0f8fd4b3988c8ac0f0abf4b306c1875dd20
mathias <mathiasbeckj@gmail.com>
2025-11-03 12:02:48 +0100

    Created empty Room endpoints

---
+++ Hotel-Hyggely-API/Application/Requests/Room/CreateRoomRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Requests.Room
+{
+    public class CreateRoomRequest
+    {
+        public required string RoomTypeName { get; set; }
+        public int Number { get; set; }
+        public int Floor { get; set; }
+        public int BedAmount { get; set; }
+    }
+}
--- Hotel-Hyggely-API/Hotel-Hyggely-API/Controllers/RoomsController.cs
+++ Hotel-Hyggely-API/Hotel-Hyggely-API/Controllers/RoomsController.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Dtos.Room;
 using Application.Dtos.RoomType;
+using Application.Requests.Room;
+using Application.Requests.Staff;
 using Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +23,36 @@ namespace Hotel_Hyggely_API.Controllers
 			var availableRooms = await roomService.GetAvailableByPeriod(fromDate, toDate);
 
 			return Ok(availableRooms);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllAsync()
+		{
+			return StatusCode(501);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetAsync(int id)
+		{
+			return StatusCode(501);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> PostAsync([FromBody] CreateRoomRequest request)
+		{
+			return StatusCode(501);
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> PutAsync([FromBody] UpdateRoomRequest request)
+		{
+			return StatusCode(501);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteAsync(int id)
+		{
+			return StatusCode(501);
 		}
 	}
 }
