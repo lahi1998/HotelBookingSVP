255a496f34125d1bc6376595fa050ebcca0c7b8f
mathias <mathiasbeckj@gmail.com>
2025-11-03 12:27:24 +0100

    Fixed bookingrepo delete method

--- Hotel-Hyggely-API/Hotel-Hyggely-API/Controllers/RoomTypesController.cs
+++ Hotel-Hyggely-API/Hotel-Hyggely-API/Controllers/RoomTypesController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel_Hyggely_API.Controllers
 {
-	[Authorize]
 	[Route("api/[controller]")]
     [ApiController]
     public class RoomTypesController : ControllerBase
@@ -33,24 +32,28 @@ namespace Hotel_Hyggely_API.Controllers
 			return Ok(RoomTypeDto);
 		}
 
+		[Authorize]
 		[HttpGet("{id}")]
 		public async Task<IActionResult> GetAsync(int id)
 		{
 			return StatusCode(501);
 		}
 
+		[Authorize]
 		[HttpPost]
 		public async Task<IActionResult> PostAsync([FromBody] CreateRoomTypeRequest request)
 		{
 			return StatusCode(501);
 		}
 
+		[Authorize]
 		[HttpPut]
 		public async Task<IActionResult> PutAsync([FromBody] UpdateRoomTypeRequest request)
 		{
 			return StatusCode(501);
 		}
 
+		[Authorize]
 		[HttpDelete("{id}")]
 		public async Task<IActionResult> DeleteAsync(int id)
 		{
