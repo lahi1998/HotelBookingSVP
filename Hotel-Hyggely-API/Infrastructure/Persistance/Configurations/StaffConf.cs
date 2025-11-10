7d5544e71520b079987b158ba72cf56fb915ed6d
mathias <mathiasbeckj@gmail.com>
2025-11-10 10:29:00 +0100

    Added unique index to staff username.
    Added custom exeption when creating staff, if username is already taken

---
+++ Hotel-Hyggely-API/Infrastructure/Persistance/Configurations/StaffConf.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Configurations
+{
+    public class StaffConf : IEntityTypeConfiguration<Staff>
+    {
+        public void Configure(EntityTypeBuilder<Staff> builder)
+        {
+            builder.HasIndex(r => r.UserName)
+                .IsUnique();
+        }
+    }
+}
